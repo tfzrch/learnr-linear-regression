03fa369c1fc5fa11e86e22a8297bd49182522dbb
tfzrch <41972466+tfzrch@users.noreply.github.com>
2022-03-06 08:11:22 +0000

    Add new script for next animation

---
+++ R/lr-animation-002.R
@@ -0,0 +1,7 @@
+# ------------------------------
+# linear-regression-anim-002.gif
+# ------------------------------
+# Show how gradient affects a linear regression line
+
+# Source setup script
+source(here::here("R", "lr-animation-setup.R"))
